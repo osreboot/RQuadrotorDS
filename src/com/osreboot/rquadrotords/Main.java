b26e4f0ea880121d8829dc36138884f91b17e4c3
2001259 <2001259@IHS60261440.ad.nsd.org>
2015-11-25 13:31:42 -0800

    Initial commit

---
+++ src/com/osreboot/rquadrotords/Main.java
@@ -0,0 +1,24 @@
+package com.osreboot.rquadrotords;
+
+import com.osreboot.ridhvl.display.collection.HvlDisplayModeDefault;
+import com.osreboot.ridhvl.template.HvlTemplate2D;
+
+public class Main extends HvlTemplate2D{
+
+	public Main(){
+		super(60, 1280, 720, "RQuadrotor Driver Station", new HvlDisplayModeDefault());
+	}
+
+	@Override
+	public void initialize(){
+		
+	}
+
+	@Override
+	public void update(float delta){
+		
+	}
+
+	
+	
+}
